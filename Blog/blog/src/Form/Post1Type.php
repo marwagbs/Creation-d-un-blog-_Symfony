f0e1f991a688e49f38e892cc2d1fd091494e8468
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-09-25 20:50:49 +0200

    Create Post1Type.php

---
+++ Blog/blog/src/Form/Post1Type.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Post1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->add('createdAt')
+            ->add('updatedAt')
+          
+            ->add('category', EntityType::class, [
+                "class"=>Category::class,
+                "choice_label"=>"name",
+                "multiple"=>true, //optionnel pour ManyToMany
+                "expanded"=>true //optionnel pour ManyToMany
+            ] )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
